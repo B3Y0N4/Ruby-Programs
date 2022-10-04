5a8007f1f0940a1e0baa3a03009ddf74f6ed91b3
Abduljalal lawal <72824981+B3Y0N4@users.noreply.github.com>
2022-10-04 23:30:27 +0300

    Delete tp11.rb

--- tp11.rb
+++
@@ -1,8 +0,0 @@
-# bir  ve sayinin kendisi hariç sayilarin tam bölenlerin yazan program
-print "Sayi giriniz"
-sayi = gets.chomp.to_i
-for i in 2..sayi-1 do
-    if sayi % i == 0
-        puts i
-    end
-end
