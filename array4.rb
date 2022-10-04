03745b4f32c925b50f01333b569456bd637c0b96
Abduljalal lawal <72824981+B3Y0N4@users.noreply.github.com>
2022-10-04 22:44:06 +0300

    Delete array4.rb

--- array4.rb
+++
@@ -1,7 +0,0 @@
-arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
-arr.delete_if {|x| x.start_with?("s",("w"))}
-a = ['white snow', 'winter wonderland', 'melting ice',
-    'slippery sidewalk', 'salted roads', 'white trees']
-a = a.map { |pairs| pairs.split }
-a = a.flatten
-p a
