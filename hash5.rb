b7d642863d1a617ecf8f8f47f82eab5d6ce14777
Abduljalal lawal <72824981+B3Y0N4@users.noreply.github.com>
2022-10-04 23:05:00 +0300

    Delete hash5.rb

--- hash5.rb
+++
@@ -1,9 +0,0 @@
-h = {}
-for i in 1..2 do
-    print "Öğrencinin ismi giriniz: "
-    isim = gets.chomp
-    print "Öğrencinin Notunu giriniz: "
-     note = gets.chomp.to_i 
-    h[isim] = note
-end
-print h.sort
