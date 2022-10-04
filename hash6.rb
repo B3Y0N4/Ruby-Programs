8ef84f04cac09b67b44a15f6c786a8ca18d8ace1
Abduljalal lawal <72824981+B3Y0N4@users.noreply.github.com>
2022-10-04 23:05:33 +0300

    Delete hash6.rb

--- hash6.rb
+++
@@ -1,9 +0,0 @@
-anonyms_word = {:spor => "oussama", :right => "left", :tall => "short"}
-print "Kelime Giriniz: "
-kelime = gets.chomp
-anonyms_word.map { |key,value|
- if key == kelime 
-    print "#{kelime}nin ziti #{value}"
- end 
-
-}
