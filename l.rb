4cb698077c59e036eab8dff1cd4053e1c0078152
Abduljalal lawal <72824981+B3Y0N4@users.noreply.github.com>
2022-10-04 23:07:21 +0300

    Delete l.rb

--- l.rb
+++
@@ -1,9 +0,0 @@
-def factorial(number)
-    result = number 
-    while number > 1
-        result = result * (number  - 1)
-        number = number - 1
-    end 
-    return result 
-end 
-puts factorial(1000000000)
